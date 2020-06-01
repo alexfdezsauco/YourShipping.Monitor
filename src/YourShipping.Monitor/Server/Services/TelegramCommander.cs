6c1d08e1a145e54f6cc7444e5af2682c5cc63695
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-06-01 01:09:37 -0400

    Improve command procesing

--- src/YourShipping.Monitor/Server/Services/TelegramCommander.cs
+++ src/YourShipping.Monitor/Server/Services/TelegramCommander.cs
@@ -1,8 +1,11 @@
 namespace YourShipping.Monitor.Server.Services
 {
     using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Eventing.Reader;
     using System.Linq;
     using System.Text;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
     using Microsoft.Extensions.DependencyInjection;
@@ -21,15 +24,23 @@ namespace YourShipping.Monitor.Server.Services
 
     public class TelegramCommander : ITelegramCommander
     {
+        private readonly Regex commandPattern = new Regex(
+            "/([^\\s]+).+",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        private readonly Dictionary<string, Func<Message, Task>> commands =
+            new Dictionary<string, Func<Message, Task>>();
+
         private readonly IServiceProvider serviceProvider;
 
         private readonly ITelegramBotClient telegramBotClient;
 
-        // private User user;
         public TelegramCommander(ITelegramBotClient telegramBotClient, IServiceProvider serviceProvider)
         {
             this.telegramBotClient = telegramBotClient;
             this.serviceProvider = serviceProvider;
+
+            this.commands.Add("search", this.Search);
         }
 
         public void Start()
@@ -40,10 +51,8 @@ namespace YourShipping.Monitor.Server.Services
 
         private async Task ProcessAsync(Message message)
         {
-            var unitOfWork = this.serviceProvider.GetService<IUnitOfWork>();
-            var productsScrapper = this.serviceProvider.GetService<IMultiEntityScrapper<Product>>();
-            var userRepository = unitOfWork.GetRepository<User, int>();
-            var storeRepository = unitOfWork.GetRepository<Store, int>();
+            var serviceScope = this.serviceProvider.CreateScope();
+            var userRepository = serviceScope.ServiceProvider.GetService<IRepository<User, int>>();
 
             var chatUsername = message.Chat.Username;
 
@@ -52,65 +61,86 @@ namespace YourShipping.Monitor.Server.Services
             {
                 if (message.Type == MessageType.Text)
                 {
-                    // Log.Information(
-                    // "Location: {Latitude}, {Longitude}",
-                    // message?.Location?.Latitude,
-                    // message?.Location?.Longitude);
-                    var splitText = message.Text.Split(' ');
-                    if (splitText.Length > 1)
+                    var match = this.commandPattern.Match(message.Text);
+                    if (match.Success)
                     {
-                        var trim = splitText[1].Trim();
-                        var keywordList = new[] { trim };
-
-                        var found = false;
-                        foreach (var storedStore in storeRepository.All())
+                        var command = match.Groups[1].Value;
+                        if (this.commands.TryGetValue(command, out var task))
                         {
-                            if (storedStore.IsEnabled)
+                            await task(message);
+                        }
+                        else
+                        {
+                            await this.telegramBotClient.SendTextMessageAsync(message.Chat.Id, "Try another command");
+                        }
+                    }
+                    else
+                    {
+                        await this.telegramBotClient.SendTextMessageAsync(
+                            message.Chat.Id,
+                            "I don't understand you. Please try again");
+                    }
+                }
+            }
+        }
+
+        private async Task Search(Message message)
+        {
+            var storeRepository =
+                this.serviceProvider.CreateScope().ServiceProvider.GetService<IRepository<Store, int>>();
+            var productsScrapper = this.serviceProvider.GetService<IMultiEntityScrapper<Product>>();
+
+            var match = Regex.Match(message.Text, @"/([^\s]+)\s+(""[^""]+""|[^\s]+)(\s+(in)\s+(.+))?", RegexOptions.IgnoreCase);
+            if (match.Success)
+            {
+                var keywords = match.Groups[2].Value.Trim('"').Split(',');
+
+                var found = false;
+                foreach (var storedStore in storeRepository.All())
+                {
+                    if (storedStore.IsEnabled)
+                    {
+                        foreach (var keyword in keywords)
+                        {
+                            var url = storedStore.Url.Replace(
+                                "/Products?depPid=0",
+                                $"/Search.aspx?keywords={keyword}&depPid=0");
+
+                            await foreach (var product in productsScrapper.GetAsync(url, true))
                             {
-                                foreach (var keyword in keywordList)
-                                {
-                                   
-                                    var url = storedStore.Url.Replace(
-                                        "/Products?depPid=0",
-                                        $"/Search.aspx?keywords={keyword}&depPid=0");
-                                    
-                                    await foreach (var product in productsScrapper.GetAsync(url, true))
-                                    {
-                                        var messageStringBuilder = new StringBuilder();
-                                        messageStringBuilder.AppendLine($"*Search result for* _{keyword}_");
-                                        messageStringBuilder.AppendLine($"*Name:* _{product.Name}_");
-                                        messageStringBuilder.AppendLine(
-                                            $"*Price:* _{product.Price} {product.Currency}_");
-                                        messageStringBuilder.AppendLine($"*Link:* [{product.Url}]({product.Url})");
-                                        messageStringBuilder.AppendLine($"*Store:* _{product.Store}_");
-                                        messageStringBuilder.AppendLine($"*Department:* _{product.Department}_");
-                                        messageStringBuilder.AppendLine(
-                                            $"*Category:* _{product.DepartmentCategory}_");
+                                var messageStringBuilder = new StringBuilder();
+                                messageStringBuilder.AppendLine($"*Search result for* _{keyword}_");
+                                messageStringBuilder.AppendLine($"*Name:* _{product.Name}_");
+                                messageStringBuilder.AppendLine($"*Price:* _{product.Price} {product.Currency}_");
+                                messageStringBuilder.AppendLine($"*Link:* [{product.Url}]({product.Url})");
+                                messageStringBuilder.AppendLine($"*Store:* _{product.Store}_");
+                                messageStringBuilder.AppendLine($"*Department:* _{product.Department}_");
+                                messageStringBuilder.AppendLine($"*Category:* _{product.DepartmentCategory}_");
 
-                                        await this.telegramBotClient.SendTextMessageAsync(
-                                            message.Chat.Id,
-                                            messageStringBuilder.ToString(),
-                                            ParseMode.Markdown);
-                                        found = true;
-                                    }
-
-                                }
+                                await this.telegramBotClient.SendTextMessageAsync(
+                                    message.Chat.Id,
+                                    messageStringBuilder.ToString(),
+                                    ParseMode.Markdown);
+                                found = true;
                             }
                         }
-
-                        if (!found)
-                        {
-                            var messageStringBuilder = new StringBuilder();
-                            messageStringBuilder.AppendLine($"*Products not found for command* {message.Text}");
-                            await this.telegramBotClient.SendTextMessageAsync(
-                                message.Chat.Id,
-                                messageStringBuilder.ToString(),
-                                ParseMode.Markdown);
-                        }
-
                     }
-
                 }
+                
+
+                if (!found)
+                {
+                    var messageStringBuilder = new StringBuilder();
+                    messageStringBuilder.AppendLine($"*Products not found for command* {message.Text}");
+                    await this.telegramBotClient.SendTextMessageAsync(
+                        message.Chat.Id,
+                        messageStringBuilder.ToString(),
+                        ParseMode.Markdown);
+                }
+            }
+            else
+            {
+                await this.telegramBotClient.SendTextMessageAsync(message.Chat.Id, "I don't understand you, please repeat");
             }
         }
 
